fd4abb1ab99f261b6e0bd968092cd628cf2a6c3d
RossDavidBrown <81567432+RossDavidBrown@users.noreply.github.com>
2022-03-29 14:45:05 +0100

    Content (#2)
    * Update CHIS-funding-v3.html
    
    * Second wave changes
    
    Launch and end task pages, plus service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Care Homes Information Service',
+  serviceName: 'Check care home information',
 
   // Default port that prototype runs on
   port: '3000',
